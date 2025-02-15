ce8895ca1d0641d79790cf3837150c68e80785e3
Hayama0024 <hayamareizo11@gmail.com>
2025-02-15 18:10:40 +0000

    puts

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #includ <stdio.h>
 
-int main (void)
+int main(void)
 {
-	puts ("\"Programming is like building a multilingual puzzle);
+	puts ("\"Programming is like building a multilingual puzzle");
         
-	return 0;
+	return (0);
 }
