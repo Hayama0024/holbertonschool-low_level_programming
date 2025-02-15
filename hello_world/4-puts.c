790ea9639ddecec42d99397d18d1011657811ee5
Hayama0024 <hayamareizo11@gmail.com>
2025-02-15 18:29:45 +0000

    puts

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,11 +1,5 @@
 #include <stdio.h>
 
-/**
- * main - Entry point
- *
- * Description: A simple program that prints a message.
- * Return: Always 0 (Success)
- */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
