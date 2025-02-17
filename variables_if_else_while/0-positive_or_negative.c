3ac46555e327b0b9ef72d9f85a40ccd472edb1f5
Hayama0024 <hayamareizo11@gmail.com>
2025-02-17 15:24:10 +0000

    if else

---
+++ variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main-Entry point
+ *
+ * return: Always 0 (Suces)
+ */
+int main(void)
+{
+	int n;
+
+	if (n > 0)
+	{
+		printf("is positive\n");
+	}
+        else
+	{
+		if (n = 0)
+		{
+			printf("is zero\n");
+		}
+        	else
+		{
+			if (n < 0)
+			{
+				printf("is negative\n");
+			}
+		}
+        return 0;
+	}
+}
