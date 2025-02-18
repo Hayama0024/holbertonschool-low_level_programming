bc701af2125a63aab6cf6707c10579841b585851
Hayama0024 <hayamareizo11@gmail.com>
2025-02-18 14:27:01 +0000

    if

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -11,17 +11,17 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
-	if (n > 5)
+	if (n % 10 > 5)
 	{
-		printf("Last diget of %d is\n and is greater than 5", n);
+		printf("Last diget of %d is %d and is greater than 5\n", n, n % 10);
 	}
-	else if (n == 0)
+	else if (n % 10 == 0)
 	{
-		printf("Last diget of %d is\n and is zero", n);
+		printf("Last diget of %d is %d and is zero\n", n, n % 10);
 	}
 	else
 	{
-		printf("Last diget of %d is\n and is less than 6", n);
+		printf("Last diget of %d is %d and is less than 6 and not 0\n", n, n % 10);
 	}
 	return (0);
 }
