c346e1edb5ec2ed891e4fefb0b9d14ba04e0d47c
Hayama0024 <hayamareizo11@gmail.com>
2025-02-19 16:14:50 +0000

    hexicademical

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0(Success)
+ */
+int main(void)
+{
+	int num;
+	char let;
+
+	for (num = 0; num < 10; num++)
+		putchar((num % 10) + '0');
+	
+	for (let = 'a'; let <= 'f'; let++)
+		putchar(let);
+	putchar('\n');
+	return (0);
+}
