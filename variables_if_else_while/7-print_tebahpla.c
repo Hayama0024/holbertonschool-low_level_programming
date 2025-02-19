049f8becb3500ea0e4cb93fbacdb07a8b1545793
Hayama0024 <hayamareizo11@gmail.com>
2025-02-19 15:39:24 +0000

    alphabet--

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Discription: program that prints the lowercase alphabet
+ * in reverse, followed by a new line
+ *
+ * Return: Always 0(success)
+ */
+int main (void)
+{
+	char alphabet;
+
+	for (alphabet = 'z'; alphabet >= 'a'; alphabet--)
+		putchar(alphabet);
+
+	putchar('\n');
+	return (0);
+}
