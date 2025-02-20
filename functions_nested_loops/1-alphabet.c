929d6bf7b976aac38d22874ee9cc70e36427054c
Hayama0024 <hayamareizo11@gmail.com>
2025-02-20 17:09:14 +0000

    prototye

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -4,12 +4,11 @@
  *
  * Return: Always 0(Success)
  */
-void print_alphabet(void);
+void print_alphabet(void)
 {
-	char letter;
+	char c;
 
-	for (letter = 'a'; letter <= 'z'; letter++)
-		_putchar(letter);
-	_putchar("\n");
-	return (0);
+	for (c = 'a'; c <= 'z'; c++)
+		_putchar(c);
+	_putchar('\n');
 }
