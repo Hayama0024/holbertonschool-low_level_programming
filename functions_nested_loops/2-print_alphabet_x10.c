08367e711034c4d6aeb8142f22fc5dfde858747f
Hayama0024 <hayamareizo11@gmail.com>
2025-02-20 20:59:05 +0000

    puff

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -6,11 +6,11 @@
  */
 void print_alphabet_x10(void)
 {
-	int i; j;
+	int i, j;
 
-	for (i = 0, i < 10, i++)
+	for (i = 0; i < 10; i++)
 	{
-		for (j = 'a', j <= 'z', j++)
+		for (j = 'a'; j <= 'z'; j++)
 		{
 			_putchar(j);
 		}
