a73e7b5a241ef0afa18d63c47897a53edb555d04
Hayama0024 <hayamareizo11@gmail.com>
2025-02-21 15:29:07 +0000

    puff

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _abs - print absolute value of a number
+ * @n: integer arg
+ * Return: abs number
+ */
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		return (-n);
+	}
+	else if (n > 0)
+	{
+		return (n);
+	}
+	else
+	{
+		return (0);
+	}
+}
