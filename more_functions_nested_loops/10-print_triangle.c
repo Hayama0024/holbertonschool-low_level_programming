1f1fa8c06ad64b71c1e066b9db15e57013b40a3d
Hayama0024 <hayamareizo11@gmail.com>
2025-02-24 23:21:35 +0000

    FCK triangle

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_triangle - prints triangle
+ * @size: size of triangle
+ */
+void print_triangle(int size)
+{
+	int i, j;
+
+	for (i = 1; i <= size; i++)
+	{
+		for (j = 1; j <= i; j++)
+		{
+			for (j = 1; j <= size - i; j++)
+			{
+				_putchar(' ');
+			}
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+}
