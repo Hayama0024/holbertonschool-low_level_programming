300d890bfcff30b45ef8b7f4085932a7e9a3f67e
Hayama0024 <hayamareizo11@gmail.com>
2025-02-24 13:54:40 +0000

    slash x2

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -10,9 +10,10 @@ void print_diagonal(int n)
 	for (i = 0; i < n; i++)
 	{
 		if (n > 0)
-			_putchar('\\');
+			_putchar(' ');
 		else
 			_putchar('\n');
 	}
+	_putchar('\\');
 	_putchar('\n');
 }
