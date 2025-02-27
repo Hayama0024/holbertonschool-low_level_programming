9c8b4b7b9be823e94f67ec7bd966e582f81a6faf
Hayama0024 <hayamareizo11@gmail.com>
2025-02-27 13:41:32 +0000

    puts

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * puts - prints a char
+ * @*str: array of char
+ */
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; str[i]; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
+
