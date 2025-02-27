ad9fb59f4ced3604b7f42b383c99e89030528a51
Hayama0024 <hayamareizo11@gmail.com>
2025-02-27 19:19:13 +0000

    reverse

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print - prints letters
+ * @*s: array of char
+ */
+void print_rev(char *s)
+{
+	int len = 0;
+
+	while (s[len] != '\0')
+		len++;
+
+	while (len > 0)
+	{
+		_putchar(s[len - 1]);
+		len--;
+	}
+	_putchar('\n');
+}
