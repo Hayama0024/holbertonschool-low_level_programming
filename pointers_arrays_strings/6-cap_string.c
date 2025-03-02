afc7e3a45e6aa72e9cf3c130f0e35d1faf96a3c8
Hayama0024 <hayamareizo11@gmail.com>
2025-03-02 21:43:46 +0000

    capitalize

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * cap_string - capitalizes first letter of a word
+ * @str: string
+ *
+ * Return: str
+ */
+char *cap_string(char *str)
+{
+	int i = 0;
+	int cap = 1;
+	char sep[] = " \t\n,;.!?\"(){}";
+	int j;
+
+	while (str[i] != '\0')
+	{
+		if (cap && (str[i] >= 'a' && str[i] <= 'z'))
+		{
+			str[i] -= ('a' - 'A');
+		}
+		cap = 0;
+
+		for (j = 0; sep[j] != '\0'; j++)
+		{
+			cap = 1;
+			break;
+		}
+	i++;
+	}
+	return (str);
+}
