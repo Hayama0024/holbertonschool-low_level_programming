7251906307af1209a1631e9a7fc6cbc964894fe5
Hayama0024 <hayamareizo11@gmail.com>
2025-03-02 19:12:03 +0000

    strcat till n

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -10,13 +10,16 @@ char *_strncat(char *dest, char *src, int n)
 {
 	int i = 0, j = 0;
 
-	for (; dest[i] != '\0'; i++)
+	while (dest[i] != '\0')
+		i++;
 
+	while (j < n && src[j] != '\0')
 	{
-		for (; j < n && src[j] != '\0'; i++, j++)
 		dest[i] = src[j];
+		i++;
+		j++;
 	}
 	dest[i] = '\0';
+
 	return (dest);
 }
-
