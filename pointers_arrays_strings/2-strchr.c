9bbf4bb1081daaed8a102c71b325785b12c1c7e4
Hayama0024 <hayamareizo11@gmail.com>
2025-03-03 21:03:39 +0000

    strchr

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stddef.h>
+/**
+ * _strchr - locates a character in a string
+ * @s: string
+ * @c: character to be lacated
+ */
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+		s++;
+	}
+
+	if (*s == '\0')
+	{
+		return (s);
+	}
+	return (NULL);
+}
+
