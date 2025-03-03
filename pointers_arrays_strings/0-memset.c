e6541fad42bdc67657001d7441a1aa320c63947b
Hayama0024 <hayamareizo11@gmail.com>
2025-03-03 13:38:09 +0000

    memset

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -1,6 +1,9 @@
 #include "main.h"
 /**
  * _memset - fills memory with constant byte
+ * @s: string
+ * @b: specifique byte
+ * @n: number of byte
  */
 char *_memset(char *s, char b, unsigned int n)
 {
