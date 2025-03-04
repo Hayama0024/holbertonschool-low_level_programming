8949d982928308cc8237bec84fec597a4560494f
Hayama0024 <hayamareizo11@gmail.com>
2025-03-04 14:29:14 +0000

    diagsums

---
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_diagsums - prints the sums of
+ * two diagonals of a square matrix
+ * @a: pointer of string
+ * @size: legth of integer
+ */
+void print_diagsums(int *a, int size)
+{
+	int i, main_diag = 0, sec_diag = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		main_diag += a[i * size + i];
+		sec_diag += a[i * (size - 1 - i)];
+	}
+
+	printf("%d, %d\n", main_diag, sec_diag);
+}
