0207946a04e2102922a32658064beefacbce0bf5
Hayama0024 <hayamareizo11@gmail.com>
2025-03-06 23:47:06 +0000

    finally

---
+++ recursion/main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - Entry point to test _print_rev_recursion
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    _print_rev_recursion("\nColton Walker");
+    return (0);
+}
+
