22205f190dc94db7bb8ccdfaaf5fc9787d7392fd
Hayama0024 <hayamareizo11@gmail.com>
2025-03-11 15:02:42 +0000

    allocate and exit

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * malloc_checked - allocate memory with malloc
+ * @b: integer
+ * @ptr: pointer
+ * Return: new space available
+ * code 98 if not
+ */
+void *malloc_checked(unsigned int b)
+{
+	int *ptr;
+
+	ptr = malloc(b);
+
+	if (ptr == NULL)
+	{
+		exit(98);
+	}
+	return (ptr);
+}
