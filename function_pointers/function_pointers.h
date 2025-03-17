501fc0bcd741354b59e8167afa10c760bb9f1fa0
Hayama0024 <hayamareizo11@gmail.com>
2025-03-17 21:04:23 +0000

    ummm

--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -1,6 +1,6 @@
 #ifndef PROTOTYPES_H
 #define PROTOTYPES_H
-
+#include <stddef.h>
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
