524d749a8eeaf14cef822f62a700a518846da74d
Hayama0024 <hayamareizo11@gmail.com>
2025-03-19 01:23:29 +0000

    fixed errors

---
+++ variadic_functions/1-print_numbers.c
@@ -0,0 +1,36 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+/**
+ * print_numbers - prints number by variadic functions
+ * @separator: separates the number
+ * @n: number of integer to print
+ * Return: void
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	int num;
+
+	if (n == 0)
+	{
+		printf("\n");
+		return;
+	}
+
+	va_list args;
+	va_start(args, n);
+
+	for (i = 0; i < n; i++)
+	{
+		num = va_arg(args, int);
+		printf("%d", num);
+
+		if (i < n - 1 && separator != NULL)
+		{
+			printf("%s", separator);
+		}
+	}
+	va_end(args);
+	printf("\n");
+}
