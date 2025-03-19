8f5fa7a13763718470d3c85b4eb19879c4619b40
Hayama0024 <hayamareizo11@gmail.com>
2025-03-19 08:59:57 +0000

    fixed betty problem

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -9,7 +9,6 @@
 void print_strings(const char *separator, const unsigned int n, ...)
 {
 	unsigned int i;
-	char *str;
 	va_list args;
 
 	if (n == 0)
@@ -22,15 +21,21 @@ void print_strings(const char *separator, const unsigned int n, ...)
 
 	for (i = 0; i < n; i++)
 	{
-		 char *str = va_arg(args, char *);
+		char *str = va_arg(args, char *);
 
-		 if (str == NULL)
-			 printf("(nil)");
-		 else
-			 printf("%s", str);
+		if (str == NULL)
+		{
+			printf("(nil)");
+		}
+		else
+		{
+			printf("%s", str);
+		}
 
-		 if (i < n -1 && separator != NULL)
-			 printf("%s", separator);
+		if (i < n - 1 && separator != NULL)
+		{
+			printf("%s", separator);
+		}
 	}
 	va_end(args);
 	printf("\n");
