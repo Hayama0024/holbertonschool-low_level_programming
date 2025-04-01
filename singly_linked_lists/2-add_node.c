0f287d4a12aa59a0d8ca9005003e2cb14a952cee
Hayama0024 <hayamareizo11@gmail.com>
2025-04-01 07:59:51 +0000

    fix betty errors

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -3,19 +3,26 @@
 #include <stdlib.h>
 #include "lists.h"
 /**
+ * _strlen - mesures the length of sting
+ * @s: string
+ * Return: the length
+ */
+
+unsigned int _strlen(const char *s)
+{
+	unsigned int len = 0;
+
+	while (s[len] != '\0')
+		len++;
+	return (len);
+}
+/**
  * add_node - adds a new node at the beginning of a list_t list
  * @head: double pointer to the head of the list
  * @str: string to be duplicated into the new node
  *
  * Return: the address of the new element, or NULL if it failed
  */
-unsigned int _strlen(const char *s)
-{
-	unsigned int len = 0;
-	while (s[len] != '\0')
-		len++;
-	return len;
-}
 
 list_t *add_node(list_t **head, const char *str)
 {
