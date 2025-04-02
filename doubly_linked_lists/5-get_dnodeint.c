5e891759bf605359c476f7adce0bac8f4786fe0e
Hayama0024 <hayamareizo11@gmail.com>
2025-04-02 22:22:25 +0000

    task5

--- doubly_linked_lists/5-get_dnodeint.c
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -8,7 +8,7 @@
  */
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
-	dlistint_ *current = head;
+	dlistint_t *current = head;
 	unsigned int count = 0;
 
 	while (current != NULL)
