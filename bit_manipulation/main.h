d7eb350e79206f74c71d5a1d1a7ec17d8bac7fad
Hayama0024 <hayamareizo11@gmail.com>
2025-04-05 15:33:57 +0000

    fix error

--- bit_manipulation/main.h
+++ bit_manipulation/main.h
@@ -9,4 +9,9 @@
 	int _sqrt_recursion(int n);
 	int is_prime_number(int n);
 	char *create_array(unsigned int size, char c);
+	unsigned int binary_to_uint(const char *b);
+	void print_binary(unsigned long int n);
+	int set_bit(unsigned long int *n, unsigned int index);
+	int clear_bit(unsigned long int *n, unsigned int index);
+	unsigned int flip_bits(unsigned long int n, unsigned long int m);
 #endif
